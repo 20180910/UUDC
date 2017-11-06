b75f05a41677e6b868bb7ad15a258112e81fa853
Administrator <2380253499@qq.com>
2017-11-06 18:06:13 +0800

    项目文件

---
+++ app/src/main/java/com/sk/uudc/module/my/activity/MyBalanceActivity.java
@@ -0,0 +1,33 @@
+package com.sk.uudc.module.my.activity;
+
+import android.view.View;
+
+import com.sk.uudc.R;
+import com.sk.uudc.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/11/6.
+ */
+
+public class MyBalanceActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        setAppTitle("我的余额");
+        return R.layout.act_my_balance;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onViewClick(View v) {
+
+    }
+}
